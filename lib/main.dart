79cdb6cfbbe948d5f2efd88dd81d665de6501f69
Samuel Otterman <sjotterman@gmail.com>
2020-10-03 21:00:03 -0400

    handle tapping or submitting field

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,85 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class Note {
+  String name;
+  String date;
+
+  Note({this.name, this.date});
+}
+
+List<Note> fakeNotes = [
+  Note(name: 'Meeting notes', date: '2020-10-01'),
+  Note(name: 'grocery list', date: '2020-09-15'),
+  Note(name: 'todo list', date: '2020-09-01'),
+  Note(name: 'app ideas', date: '2020-08-01'),
+  Note(name: 'favorite foods', date: '2020-07-03'),
+  Note(name: 'packing list', date: '2020-07-03'),
+  Note(name: 'motorcycles', date: '2020-07-03'),
+  Note(name: 'not a real note', date: '2020-07-03'),
+  Note(name: 'this is just for extra items', date: '2020-07-03'),
+  Note(name: 'doesn\'t matter what this is called', date: '2020-07-03'),
+  Note(name: 'another note', date: '2020-07-03'),
+  Note(name: 'another note', date: '2020-07-03'),
+];
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(hintText: 'Create or find a note'),
+              onChanged: (text) {
+                print("Search field value: $text");
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: fakeNotes.length,
+                  itemBuilder: (context, index) {
+                    return NoteListItem(note: fakeNotes[index]);
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NoteListItem extends StatelessWidget {
+  final Note note;
+
+  const NoteListItem({Key key, this.note}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        child: ListTile(
+          title: Text(note.name),
+          subtitle: Text(note.date),
+          dense: true,
+        ),
+      ),
+    );
+  }
+}
