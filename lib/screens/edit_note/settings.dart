9232023813aca778cf0f69a71d4afec5bc9aae8f
Samuel Otterman <sjotterman@gmail.com>
2020-11-15 12:53:36 -0500

    non-functional settings page

--- lib/screens/edit_note/settings.dart
+++ lib/screens/edit_note/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:otter_notes/services/note_service.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -6,10 +7,42 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  String _noteDir = '...';
+
+  void initState() {
+    super.initState();
+    NoteService().noteDir.then((dir) {
+      print('updated dir: $dir');
+      setState(() {
+        _noteDir = dir;
+      });
+    });
+  }
+
+  void _onPressEditDir() {
+    // TODO: open file picker
+    // set new directory
+    print('pressed edit dir');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('settings'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text('Notes Directory'),
+              Text(_noteDir),
+              ElevatedButton.icon(
+                onPressed: _onPressEditDir,
+                label: Text('Edit'),
+                icon: Icon(Icons.edit),
+              )
+            ],
+          ),
+        ));
   }
 }
